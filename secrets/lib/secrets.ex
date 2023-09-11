25c45210d276d8f348265debf5d92fae547052bb
Gabo <gabrielomar2809@gmail.com>
2023-09-11 03:06:09 -0700

    Secrets Iter. 2

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,29 +1,15 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    &(&1 + secret)
-  end
+  def secret_add(secret), do: &(&1 + secret)
 
-  def secret_subtract(secret) do
-    &(&1 - secret)
-  end
+  def secret_subtract(secret), do: &(&1 - secret)
 
-  def secret_multiply(secret) do
-    &(&1 * secret)
-  end
+  def secret_multiply(secret), do: &(&1 * secret)
 
-  def secret_divide(secret) do
-    &div(&1, secret)
-  end
+  def secret_divide(secret), do: &div(&1, secret)
 
-  def secret_and(secret) do
-    fn x -> Bitwise.band(x, secret) end
-  end
+  def secret_and(secret), do: &Bitwise.band(&1, secret)
 
-  def secret_xor(secret) do
-    fn x -> Bitwise.bxor(x, secret) end
-  end
+  def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
 
-  def secret_combine(secret_function1, secret_function2) do
-    fn x -> secret_function2.(secret_function1.(x)) end
-  end
+  def secret_combine(fn1, fn2), do: &(&1 |> then(fn1) |> then(fn2))
 end
